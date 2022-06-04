cd5a46c6b4830a55e5f269203dfb7fd9217c8ab4
ZamanMo3 <92724709+ZamanMo3@users.noreply.github.com>
2022-06-04 15:40:24 +0430

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,10 +1,30 @@
+import os
 import pyfiglet
 from termcolor2 import colored
 
+os.system('pip install pyfiglet')
+os.system('pip install termcolor2')
 
-massage = input('what would you like to print? : ')
+os.system('cls')
+
+
+chanel = '@termux_scripte'
+nexus = 'printer tools'
+# c = pyfiglet.figlet_format(chanel)
+z = pyfiglet.figlet_format(nexus)
+p = colored(chanel, ('cyan'))
+pr = colored(z, ('yellow'))
+
+print(p)
+print(pr)
+
+
+massage = input('what would you like to print? : ') 
+# massage = colored(massage, ('blue'))
 colo = input('what color? : ')
+# colo = colored(colo, ('red'))
 ascii_art = pyfiglet.figlet_format(massage)
-ascii_art = colored(ascii_art, (colo))
+as_art = colored(ascii_art, (colo))
 
-print(ascii_art)
+
+print(as_art)
